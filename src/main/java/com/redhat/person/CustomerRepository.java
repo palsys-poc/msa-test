d3ba6a278271eff1574f40d2a167722233a6250e
David <dyangcht@aim.com>
2022-03-20 16:00:23 +0800

    update

--- src/main/java/com/redhat/person/CustomerRepository.java
+++ src/main/java/com/redhat/person/CustomerRepository.java
@@ -1,10 +1,13 @@
 package com.redhat.person;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    List<Customer> findByName(String name);
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customers")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+    List<Customer> findByName(@Param("name") String name);
 
     Customer findById(long id);
 }
