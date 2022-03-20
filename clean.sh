c00377c41711c59bb7bcf3c6a4c24c3ab4fe82f1
David <dyangcht@aim.com>
2022-03-20 16:03:38 +0800

    update

---
+++ clean.sh
@@ -0,0 +1,6 @@
+oc delete deploy msa-test-git
+oc delete svc msa-test-git
+oc delete is msa-test-git
+oc delete bc msa-test-git
+oc delete secret msa-test-git-generic-webhook-secret
+oc delete secret msa-test-git-github-webhook-secret
