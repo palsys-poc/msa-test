fc21a5837dc59d173b25e333a85d2a145688ec5d
David <dyangcht@aim.com>
2022-04-04 16:40:22 +0800

    update

--- src/main/java/com/redhat/person/PersonApplication.java
+++ src/main/java/com/redhat/person/PersonApplication.java
@@ -21,37 +21,41 @@ public class PersonApplication {
 	public CommandLineRunner demo(CustomerRepository repository) {
 		return (args) -> {
 			// save a few customers
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
+			try {
+				repository.save(new Customer("Jack", "Bauer"));
+				repository.save(new Customer("Chloe", "O'Brian"));
+				repository.save(new Customer("Kim", "Bauer"));
+				repository.save(new Customer("David", "Palmer"));
+				repository.save(new Customer("Michelle", "Dessler"));
 
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
+				// fetch all customers
+				log.info("Customers found with findAll():");
+				log.info("-------------------------------");
+				for (Customer customer : repository.findAll()) {
+					log.info(customer.toString());
+				}
+				log.info("");
+
+				// fetch an individual customer by ID
+				Customer customer = repository.findById(1L);
+				log.info("Customer found with findById(1L):");
+				log.info("--------------------------------");
 				log.info(customer.toString());
+				log.info("");
+
+				// fetch customers by last name
+				log.info("Customer found with findByLastName('Bauer'):");
+				log.info("--------------------------------------------");
+				repository.findByName("Jack").forEach(bauer -> {
+					log.info(bauer.toString());
+				});
+				// for (Customer bauer : repository.findByLastName("Bauer")) {
+				// log.info(bauer.toString());
+				// }
+				log.info("");
+			} catch (Exception e) {
+				log.info("duplicate keys or cannot find a specific customer");
 			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			Customer customer = repository.findById(1L);
-			log.info("Customer found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByName("Jack").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			// log.info(bauer.toString());
-			// }
-			log.info("");
 		};
 	}
 }
